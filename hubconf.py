30c9ac2488883837af6dfccce945ead0c2318b09
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2024-01-18 15:38:34 +0100

    passing pretrained flag

--- hubconf.py
+++ hubconf.py
@@ -1,5 +1,5 @@
 from imagebind.models import imagebind_model
 
 def imagebind_huge(pretrained=True):
-    model = imagebind_model.imagebind_huge(pretrained=True)
+    model = imagebind_model.imagebind_huge(pretrained=pretrained)
     return model
